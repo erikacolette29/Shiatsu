0f6537c3a670e209f6bb82cbedfce2d807526a72
erikacolette <erikatidwell.c@gmail.com>
2020-12-17 13:24:58 -0500

    friend and unfriend working and show on index

--- controllers/users.js
+++ controllers/users.js
@@ -8,13 +8,17 @@ module.exports = {
    showSelfEval,
  createEval,
  showTheory,
- 
+ userList,
+ showUdetails,
+ addFriend,
+ unFriend,
 }
 
 function index(req,res) {
-User.find({})
-.then((users)=> {
-res.render("users/index", {title: "User Index", users, user: req.user})
+User.findById(req.user._id)
+.populate("friends")
+.then((user)=> {
+res.render("users/index", {title: "User Index", user})
     })
 }
 
@@ -52,5 +56,46 @@ User.findById(req.params.id, function(err, user){
 function showTheory(req,res){
     User.find({}).then((users)=> {
         res.render("users/theory", {title: "Theory", user: req.user, users})
+    })
+}
+
+
+// user list
+
+function userList(req,res){
+        User.find({}).then((users) => {
+          res.render("users/userlist", { title: "User list", user: req.user, users });
+        });
+      
+}
+
+function showUdetails(req,res){
+    User.findById(req.params.id)
+    .then((userInfo) =>{
+        res.render("users/usershow", {
+        title: "User Details",
+        userInfo, 
+        user: req.user
+        })
+})
+}
+
+
+//friend and unfriend
+
+function addFriend(req,res){
+req.user.friends.push(req.params.id)
+req.user.save()
+.then(() =>{
+   res.redirect(`/users/${req.params.id}`) 
+})
+}
+
+function unFriend(req,res){
+    let idx = req.user.friends.indexOf(req.params.id)
+    req.user.friends.splice(idx, 1)
+    req.user.save()
+    .then(() =>{
+        res.redirect(`/users/${req.params.id}`) 
     })
 }
--- routes/users.js
+++ routes/users.js
@@ -13,6 +13,18 @@ router.put("/:id/neweval", isLoggedIn, userCtrl.createEval)
 
 router.get("/theory", isLoggedIn, userCtrl.showTheory)
 
+
+//userlist
+router.get("/userlist", isLoggedIn, userCtrl.userList);
+router.get("/:id", isLoggedIn, userCtrl.showUdetails)
+
+router.get("/:id/friend", isLoggedIn, userCtrl.addFriend)
+router.get("/:id/unfriend", isLoggedIn, userCtrl.unFriend)
+
+
+
+
+
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
     res.redirect("/auth/google");
